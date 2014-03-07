74f606993223e2c2cfb55e2512ee64b988f2a191
Michael Li <columbia.cs@gmail.com>
2014-03-07 10:52:00 -0500

    template setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Scratch::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'post#index'
+  root 'posts#index'
 
-  resources :post
+  resources :posts
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
